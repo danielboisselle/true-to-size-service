08c7a790ca960e881fe343431bf415377097adf7
Daniel Boisselle <daniel.r.boisselle@gmail.com>
2019-06-12 20:18:15 -0400

    add intial getAll, createOne, deleteOne and addEntry methods

--- src/trueToSize/trueToSize.controller.js
+++ src/trueToSize/trueToSize.controller.js
@@ -1,3 +1,23 @@
 module.exports = model => ({
+  getAll() {
+    return model.findAll().then(res => res);
+  },
+  createOne() {
+    return model.create();
+  },
+  deleteOne(id) {
+    return model.findByPk(id)
+      .then(inst => inst.destroy());
+  },
+  addEntry(id, entry) {
+    return model.findByPk(id)
+      .then((inst) => {
+        const updatedEntries = [...inst.entries, entry];
 
+        return inst.update({
+          entries: updatedEntries,
+          average: (updatedEntries.reduce((a, b) => a + b) / updatedEntries.length),
+        });
+      });
+  },
 });
