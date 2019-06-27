989e5ddf2a0a988de1505ae25941bedd58a3e1f0
Daniel Boisselle <daniel.r.boisselle@gmail.com>
2019-06-27 15:59:58 -0400

    update:
    addEntry updates entriesSum when adding new entry
    impliment removeEntry method

--- src/controllers/trueToSize.controller.js
+++ src/controllers/trueToSize.controller.js
@@ -35,12 +35,56 @@ module.exports = model => ({
           throw new NotFoundError();
         }
 
-        const updatedEntries = [...inst.entries, entry];
+        const {
+          entries,
+          entriesSum,
+        } = inst;
+
+        const updatedEntries = [...entries, entry];
+
+        const updatedEntriesSum = entry + entriesSum;
 
         return inst.update({
           entries: updatedEntries,
           average: averageEntries(updatedEntries),
+          entriesSum: updatedEntriesSum,
         });
       });
   },
+  removeEntry(id, entry) {
+    const validEntries = [1, 2, 3, 4, 5];
+
+    if (!validEntries.includes(entry)) {
+      throw new ValidationError(`Possible valid entries ${validEntries}`);
+    }
+
+    return model.findByPk(id)
+      .then((inst) => {
+        if (!inst) {
+          throw new NotFoundError();
+        }
+
+        const {
+          entries,
+          entriesSum,
+        } = inst;
+
+        // Removing an entry that has not been added previously
+        if (!entries.includes(entry)) {
+          return inst;
+        }
+
+        const removeIndex = entries.indexOf(entry);
+
+        const updatedEntries = [...entries].splice(removeIndex, 1);
+
+        const updatedEntriesSum = entriesSum - entry;
+
+        return inst.update({
+          entries: updatedEntries,
+          average: averageEntries(updatedEntries),
+          entriesSum: updatedEntriesSum,
+        });
+      });
+  }
 });
